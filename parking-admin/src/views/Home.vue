5abb71883532a4fcd1fc6266b53c28fa0673095f
Maple <aa694256235@qq.com>
2018-12-25 13:50:07 +0800

    login

--- parking-admin/src/views/Home.vue
+++ parking-admin/src/views/Home.vue
@@ -1,18 +1,17 @@
+<!-- home.vue -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+    };
+  },
 }
+
 </script>
+
+<style lang='scss' scoped>
+</style>
---
+++ parking-admin/src/views/login.vue
@@ -0,0 +1,94 @@
+<!-- login.vue -->
+<template>
+  <div class="login">
+    <div class="login-container">
+      <Divider><span class="login-label">管理员登陆</span></Divider>
+      <Form ref="formInline" :model="formInline" :rules="ruleInline">
+        <FormItem prop="user">
+          <Input type="text" v-model="formInline.user" placeholder="请输入用户名">
+            <Icon type="ios-person-outline" slot="prepend"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input type="password" v-model="formInline.password" placeholder="请输入密码">
+            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" @click="handleSubmit('formInline')" long>登陆</Button>
+        </FormItem>
+      </Form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formInline: {
+        user: "",
+        password: ""
+      },
+      ruleInline: {
+        user: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "密码长度不能少于6位",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    handleSubmit(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.$Message.success("登陆成功!");
+        } else {
+          this.$Message.error("登陆失败!");
+        }
+      });
+    }
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.login {
+  background-image: url(../assets/images/login-bg.png);
+  background-repeat: no-repeat;
+  height: 100%;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;  
+}
+.login-container{
+  width: 400px;
+  height: 300px;
+  padding: 20px 80px 20px;
+  border: 1px solid #f1f1f1;
+  border-radius: 5px;
+  background: white;
+}
+.login-label{
+  font-weight: 600;
+  font-size: 20px;
+  color: #17233d;
+}
+</style>
