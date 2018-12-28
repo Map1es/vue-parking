917e504b1730599f257818edf9d59894e5a796a5
Maple <aa694256235@qq.com>
2018-12-28 10:38:02 +0800

    fix px

--- parking-app/src/main.js
+++ parking-app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/iview.js'
+import 'lib-flexible/flexible.js'
 
 Vue.config.productionTip = false
 
